699a1778b4a2f7988628ae4921212351cddb2b3c
S-Diemel <stijn.diemel@gmail.com>
2025-03-24 09:57:12 +0100

    --

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
-from waitress import serve
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('pindex.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    # Serve the app on http://127.0.0.1:8000
-    serve(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=False)
+
